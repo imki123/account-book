9adddbf6b15875d1be58a16b91a0263b6a3237c5
Hoodie(Im Geeyoung) <popping2606@gmail.com>
2022-08-01 18:32:47 +0900

    table height 수정

--- src/component/SheetTable/SheetTable.tsx
+++ src/component/SheetTable/SheetTable.tsx
@@ -78,7 +78,7 @@ export default function SheetTable({
                     <td>
                       <CommonInput
                         value={col}
-                        height='27px'
+                        height='28px'
                         onChange={(e) => handleInputChange(e, i, j)}
                       />
                     </td>
